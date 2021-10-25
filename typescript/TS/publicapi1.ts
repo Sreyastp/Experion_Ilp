ca37d1334f0714d479e93d377719fb1161ef3b46
Sreyastp <sreyastp444@gmail.com>
2021-10-25 12:15:40 +0530

    save monday

---
+++ typescript/TS/publicapi1.ts
@@ -0,0 +1,13 @@
+//make changes in ts config.json
+// - "target": "es5"
+// - "module": "es2015"
+
+//step 1-details of the employee
+
+//step 2- declare webapi - endpoints
+
+//step 3 fetch all employees
+
+//step 4- fetch an employee 
+
+//step 5 - run Async-Promise-Await Functions
