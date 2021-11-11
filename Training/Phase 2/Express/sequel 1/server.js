29f791759dc3c9956d38aa236426f483dcccc8fd
Sreyastp <sreyastp444@gmail.com>
2021-11-12 00:06:43 +0530

    sqllite

---
+++ Training/Phase 2/Express/sequel 1/server.js
@@ -0,0 +1,37 @@
+//install --------      npm i express sequelize sqlite3 -s
+//start-----------   npm start`
+
+
+const express = require('express');
+const Sequelize = require('Sequelize');
+
+const app = express();
+const port = 8001;
+
+const connection = new Sequelize('db' , 'user' , 'pass' ,{
+    host:'localhost',
+    dialect:'sqlite',
+    storage:'db.sqllite',
+    operatorAliases: false
+})
+
+const User = connection.define('User',{
+    name: Sequelize.STRING,
+    bio:Sequelize.TEXT
+})
+
+connection.sync({
+    logging:console.log
+})
+.then(() => {
+    console.log("connection to database established successfully");
+})
+.catch(err=>{
+    console.log('unable to connect to the database',err);
+});
+
+
+app.listen(port, () => {
+    console.log('running server on port' + port);
+
+})
