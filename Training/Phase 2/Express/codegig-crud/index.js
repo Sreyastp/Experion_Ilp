f9a293a9945df2b81d58097e1b25298939ab1ede
Sreyastp <sreyastp444@gmail.com>
2021-11-12 16:49:59 +0530

    sequelize

--- Training/Phase 2/Express/codegig-crud/index.js
+++ Training/Phase 2/Express/codegig-crud/index.js
@@ -23,3 +23,22 @@ const PORT = process.env.PORT || 5000;
 db.sync().then(() => {
     app.listen(PORT, console.log(`Server started on port ${PORT}`));
 }).catch(err => console.log("Error: " + err));
+
+
+
+// //{
+//     "title": "Looking for a MERN Stack Developer",
+//     "technologies": "Mongo, Express, React and Node JS",
+//     "description": "Should have strong programming knowledge",
+//     "budget": "$40000",
+//     "contact_email": "employer@IBM.com"
+// }
+
+// {
+//     "title": "Looking for a MERN Stack Developer",
+//     "technologies": "Mongo, Express, React and Node JS",
+//     "description": "Should have strong programming knowledge",
+//     "budget": "$12000",
+//     "contact_email": "employer@IBM.com"
+// }
+// http://localhost:5000/gigs/2
